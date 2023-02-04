f391afa205f3bbfdc27496c51b9113f68094df3f
Minami926494 <1476914340@qq.com>
2023-02-04 16:41:14 +0800

    ver 1.2.1a

--- script-original.js
+++ script-original.js
@@ -1,11 +1,13 @@
-<!--
 document.onreadystatechange = function() {
   if(document.readyState == 'interactive') {
     var pc = RegExp(/windows|x11|mac/i);
+    var ibooks = RegExp(/ibooks|图书|圖書|図書/i);
     var noteLogo = document.querySelectorAll('sup');
     var noteText = document.querySelectorAll('aside');
-    noteLogo.onload = changeNote(noteLogo,noteText);
-    window.onresize = changeNote(noteLogo,noteText);
+    if(navigator.userAgent.match(ibooks) == null) {
+      noteLogo.onload = changeNote(noteLogo,noteText);
+      window.onresize = changeNote(noteLogo,noteText);
+    }
     function changeNote(a,b) {
       for(let i = 0;i < b.length;i++) {
         var notePanel = b[i].parentNode;
@@ -15,31 +17,36 @@ document.onreadystatechange = function() {
         if(href.match('#' + id)) {
           var text = a[i].parentNode;
           notePanel.classList.add('change');
-          link.href = 'javascript:void(0)';
-          b[i].querySelector('a').href = 'javascript:void(0)';
+          link.removeAttribute('href');
+          b[i].querySelector('a').removeAttribute('href');
           if(navigator.userAgent.match(pc)) {
             link.onmouseover = note;
-            link.onmouseout = function() {
-              b[i].removeAttribute('style')
-            }
+            link.onmouseout = fade
           }
           else {
-            link.onfocus = note;
-            link.onblur = function() {
-              b[i].removeAttribute('style')
-            }
+            link.ontouchstart = note;
+            link.ontouchend = fade
           }
           function note() {
-            let w = window.innerWidth;
+            b[i].removeEventListener('webkitAnimationend',z);
+            b[i].removeEventListener('animationend',z);
+            b[i].removeAttribute('style');
+            b[i].removeAttribute('class');
+            let dir = '';
+            let w = function() {
+              return document.body.clientWidth || document.documentElement.clientWidth;
+            }();
+            let h = window.innerHeight;
             let mw = a[i].offsetWidth;
             let mh = a[i].offsetHeight;
             let pw = notePanel.offsetWidth;
-            let ph = text.offsetHeight;
+            let ph = notePanel.offsetHeight;
             let left = a[i].offsetLeft;
             let top = a[i].offsetTop;
             let right = pw - left;
+            let bottom = ph - top;
             let ml = mw / 2 + left;
-            let mt = event.clientY;
+            let mt = a[i].getBoundingClientRect().top;
             if(w >= pw * 2) {
               w /= 2;
               if(ml > w) {
@@ -47,20 +54,47 @@ document.onreadystatechange = function() {
               }
             }
             let vw = ml * 100 / w;
-            if(vw > 50) {
-              b[i].style.right = right + 'px'
+            if(vw > 80) {
+              b[i].style.left = 'auto';
+              b[i].style.right = right - mw / 2 + 1 + 'px';
+              dir += 'zuo-out'
+            }
+            else if(vw > 50) {
+              b[i].style.left = 'auto';
+              b[i].style.right = right - mw / 2 - 51 + 'px';
+              dir += 'zuo'
+            }
+            else if(vw > 20) {
+              b[i].style.left = left + mw / 2 - 51 + 'px';
+              dir += 'you'
             }
             else {
-              b[i].style.left = left + mw + 'px'
+              b[i].style.left = left + mw / 2 + 1 + 'px';
+              dir += 'you-out'
             }
             let nh = b[i].offsetHeight;
-            if(mt >= nh + mh) {
-              b[i].style.top = top - nh + 'px'
+            if(mt <= nh + 10 && mt < h / 2) {
+              b[i].style.top = top + mh + 10 + 'px';
+              dir += '-xia'
             }
             else {
-              b[i].style.top = top + mh + 'px'
+              b[i].style.top = 'auto';
+              b[i].style.bottom = bottom + mh + 10 + 'px';
+              dir += '-shang'
             }
-            b[i].style.visibility = 'visible'
+            b[i].classList.add(dir);
+            b[i].style.zIndex = '1';
+            b[i].style.webkitAnimation = 'show 0.3s 1 forwards';
+            b[i].style.animation = 'show 0.3s 1 forwards'
+          }
+          function fade() {
+            b[i].style.webkitAnimation = 'fade 0.3s 1 forwards';
+            b[i].style.animation = 'fade 0.3s 1 forwards';
+            b[i].addEventListener('webkitAnimationend',z);
+            b[i].addEventListener('animationend',z)
+          }
+          function z() {
+            b[i].style.zIndex = '-1'
           }
         }
       }
@@ -99,4 +133,3 @@ document.onreadystatechange = function() {
     }
   }
 }
-//-->
