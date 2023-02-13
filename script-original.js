a8abc2fc351c0b58e6c0c7c7e82912a28fed2019
Minami926494 <1476914340@qq.com>
2023-02-13 08:04:49 +0800

    ver 1.2.1a1

--- script-original.js
+++ script-original.js
@@ -2,99 +2,102 @@ document.onreadystatechange = function() {
   if(document.readyState == 'interactive') {
     var pc = RegExp(/windows|x11|mac/i);
     var ibooks = RegExp(/ibooks|图书|圖書|図書/i);
-    var noteLogo = document.querySelectorAll('sup');
-    var noteText = document.querySelectorAll('aside');
     if(navigator.userAgent.match(ibooks) == null) {
-      noteLogo.onload = changeNote(noteLogo,noteText);
-      window.onresize = changeNote(noteLogo,noteText);
+      window.onload = changeNote;
+      window.onresize = changeNote;
     }
-    function changeNote(a,b) {
-      for(let i = 0;i < b.length;i++) {
-        var notePanel = b[i].parentNode;
-        var link = a[i].querySelector('a');
-        var href = link.href;
-        var id = b[i].id;
-        if(href.match('#' + id)) {
-          var text = a[i].parentNode;
-          notePanel.classList.add('change');
-          link.removeAttribute('href');
-          b[i].querySelector('a').removeAttribute('href');
-          if(navigator.userAgent.match(pc)) {
-            link.onmouseover = note;
-            link.onmouseout = fade
-          }
-          else {
-            link.ontouchstart = note;
-            link.ontouchend = fade
-          }
-          function note() {
-            b[i].removeEventListener('webkitAnimationend',z);
-            b[i].removeEventListener('animationend',z);
-            b[i].removeAttribute('style');
-            b[i].removeAttribute('class');
-            let dir = '';
-            let w = function() {
-              return document.body.clientWidth || document.documentElement.clientWidth;
-            }();
-            let h = window.innerHeight;
-            let mw = a[i].offsetWidth;
-            let mh = a[i].offsetHeight;
-            let pw = notePanel.offsetWidth;
-            let ph = notePanel.offsetHeight;
-            let left = a[i].offsetLeft;
-            let top = a[i].offsetTop;
-            let right = pw - left;
-            let bottom = ph - top;
-            let ml = mw / 2 + left;
-            let mt = a[i].getBoundingClientRect().top;
-            if(w >= pw * 2) {
-              w /= 2;
-              if(ml > w) {
-                ml -= w
+    function changeNote() {
+      var n = document.querySelectorAll('note');
+      for(let i = 0;i < n.length;i++) {
+        let sup = n[i].querySelectorAll('sup');
+        let aside = n[i].querySelectorAll('aside');
+        for(let j = 0;j < sup.length;j++) {
+          let link = sup[j].querySelector('a');
+          let pic = sup[j].querySelector('img');
+          let href = link.href;
+          let id = aside[j].id;
+          if(href.match('#' + id)) {
+            let text = sup[j].parentNode;
+            n[i].classList.add('change');
+            link.removeAttribute('href');
+            aside[j].querySelector('a').removeAttribute('href');
+            if(navigator.userAgent.match(pc)) {
+              link.onmouseover = note;
+              link.onmouseout = fade
+            }
+            else {
+              link.ontouchstart = note;
+              link.ontouchend = fade
+            }
+            function note() {
+              aside[j].removeEventListener('webkitAnimationend',z);
+              aside[j].removeEventListener('animationend',z);
+              aside[j].removeAttribute('style');
+              aside[j].removeAttribute('class');
+              let dir = '';
+              let w = function() {
+                return document.body.clientWidth || document.documentElement.clientWidth;
+              }();
+              let h = window.innerHeight;
+              let mw = pic.offsetWidth;
+              let mh = pic.offsetHeight;
+              let pw = n[i].offsetWidth;
+              let ph = n[i].offsetHeight;
+              let left = pic.offsetLeft;
+              let top = pic.offsetTop;
+              let right = pw - left;
+              let bottom = ph - top;
+              let ml = mw / 2 + left;
+              let mt = pic.getBoundingClientRect().top;
+              if(w >= pw * 2) {
+                w /= 2;
+                if(ml > w) {
+                  ml -= w
+                }
               }
+              let vw = ml * 100 / w;
+              if(vw > 80) {
+                aside[j].style.left = 'unset';
+                aside[j].style.right = right - mw / 2 + 1 + 'px';
+                dir += 'zuo-out'
+              }
+              else if(vw > 50) {
+                aside[j].style.left = 'unset';
+                aside[j].style.right = right - mw / 2 - 51 + 'px';
+                dir += 'zuo'
+              }
+              else if(vw > 20) {
+                aside[j].style.left = left + mw / 2 - 51 + 'px';
+                dir += 'you'
+              }
+              else {
+                aside[j].style.left = left + mw / 2 + 1 + 'px';
+                dir += 'you-out'
+              }
+              let nh = aside[j].offsetHeight;
+              if(mt <= nh + 10 && mt < h / 2) {
+                aside[j].style.top = top + mh + 10 + 'px';
+                dir += '-xia'
+              }
+              else {
+                aside[j].style.top = 'unset';
+                aside[j].style.bottom = bottom + 10 + 'px';
+                dir += '-shang'
+              }
+              aside[j].classList.add(dir);
+              aside[j].style.zIndex = '1';
+              aside[j].style.webkitAnimation = 'show 0.3s 1 forwards';
+              aside[j].style.animation = 'show 0.3s 1 forwards'
             }
-            let vw = ml * 100 / w;
-            if(vw > 80) {
-              b[i].style.left = 'auto';
-              b[i].style.right = right - mw / 2 + 1 + 'px';
-              dir += 'zuo-out'
+            function fade() {
+              aside[j].style.webkitAnimation = 'fade 0.3s 1 forwards';
+              aside[j].style.animation = 'fade 0.3s 1 forwards';
+              aside[j].addEventListener('webkitAnimationend',z);
+              aside[j].addEventListener('animationend',z)
             }
-            else if(vw > 50) {
-              b[i].style.left = 'auto';
-              b[i].style.right = right - mw / 2 - 51 + 'px';
-              dir += 'zuo'
+            function z() {
+              aside[j].style.zIndex = '-1'
             }
-            else if(vw > 20) {
-              b[i].style.left = left + mw / 2 - 51 + 'px';
-              dir += 'you'
-            }
-            else {
-              b[i].style.left = left + mw / 2 + 1 + 'px';
-              dir += 'you-out'
-            }
-            let nh = b[i].offsetHeight;
-            if(mt <= nh + 10 && mt < h / 2) {
-              b[i].style.top = top + mh + 10 + 'px';
-              dir += '-xia'
-            }
-            else {
-              b[i].style.top = 'auto';
-              b[i].style.bottom = bottom + 10 + 'px';
-              dir += '-shang'
-            }
-            b[i].classList.add(dir);
-            b[i].style.zIndex = '1';
-            b[i].style.webkitAnimation = 'show 0.3s 1 forwards';
-            b[i].style.animation = 'show 0.3s 1 forwards'
-          }
-          function fade() {
-            b[i].style.webkitAnimation = 'fade 0.3s 1 forwards';
-            b[i].style.animation = 'fade 0.3s 1 forwards';
-            b[i].addEventListener('webkitAnimationend',z);
-            b[i].addEventListener('animationend',z)
-          }
-          function z() {
-            b[i].style.zIndex = '-1'
           }
         }
       }
