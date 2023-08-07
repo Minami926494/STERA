3a6c46d6471ec240d3584f6ea94f51c062bf28ea
Minami926494 <1476914340@qq.com>
2023-08-07 10:51:08 +0800

    ver 1.4.0

--- STERA-X/STERAEPUB/subfont_core.py
+++ STERA-X/STERAEPUB/subfont_core.py
@@ -15,19 +15,16 @@ except ImportError:
     from bookenv_core import book, getbsn, first
 
 # 字体子集化
-white_clear, html_style, html_clear, html_link, html_line, css_clssel, css_idsel, css_regsel, font_split = compile(r'[\s　]+'), compile(r'(?s)<style[^>]*?>((?:(?!</style>).)*?)</style>'), compile(r'(?s)(?:<!--(?:(?!-->).)+-->|<script(?:(?!</script>).)*?</script>|<style(?:(?!</style>).)*?</style>)'), compile(
-    r'<link[^>]*?href="([^"]+)"'), compile(r'font-family:\s*([^;\"\']+?)\s*(!important)?\s*(?:[;\"\']|$)'), compile(r'\.[A-Za-z\d*]+\s*$'), compile(r'#[A-Za-z\d*]+\s*$'), compile(r'\]\s*$'), compile(r'[\s,]+')
+white_clear, html_line, css_splitsel, css_clssel, css_idsel, css_regsel = compile(r'[\s　]+'), compile(
+    r'font-family:\s*([^;\"\']+?)\s*(!important)?\s*(?:[;\"\']|$)'), compile(r'[ >]+'), compile(r'\.[A-Za-z\d*]+\s*$'), compile(r'#[A-Za-z\d*]+\s*$'), compile(r'\]\s*$')
 
 
 def subfont(bk: book):
     print('\n字体子集化……')
     GLYPH, ELEM, FONT, CHANGED = {}, {}, {}, {}
-
-    def fmlparser(ele: book.elem, data: str | None = None):
-        if not data:
-            data = ele.read()
+    for ele in bk.iter('css'):
         sheet, ele.imp, ele.font, ele.sel = CSSParser(
-            loglevel='CRITICAL', parseComments=False, validate=False).parseString(data).cssRules, [], {}, {}
+            loglevel='CRITICAL', parseComments=False, validate=False).parseString(ele.read()).cssRules, [], {}, {}
         for i in sheet:
             if i.type == 3:
                 imp = bk.get(bsn=first(getbsn(i.href)))
@@ -37,21 +34,20 @@ def subfont(bk: book):
                 fml = i.style.fontFamily
                 if fml:
                     if i.type == 5:
-                        src = bk.get(bsn=first(getbsn(i.style.src)))
-                        if src and src not in ele.font:
-                            ele.font[fml.split(',', 1)[0].strip('\'" ')] = src
+                        src = [bk.get(bsn=font) for font in getbsn(i.style.src) if font]
+                        if src:
+                            ele.font[fml.strip('\'" ')] = src
                     elif i.type == 1:
                         sel = i.selectorText
-                        if css_clssel.search(sel) or css_regsel.search(sel):
-                            im = 1
-                        elif css_idsel.search(sel):
-                            im = 2
-                        else:
+                        for part in css_splitsel.split(sel):
                             im = 0
-                        ele.sel[sel] = (tuple(j.strip('\'" ') for j in fml.split(
-                            ',')), im+4 if i.style.getPropertyPriority('font-family') else im)
-    for ele in bk.iter('css'):
-        fmlparser(ele)
+                            if part.startswith(tuple('abcdefghijklmnopqrstuvwxyz*')):
+                                im += 1
+                            if css_clssel.search(part) or css_regsel.search(part):
+                                im += 10
+                            if css_idsel.search(part):
+                                im += 100
+                        ele.sel[sel] = (tuple(j.strip() for j in fml.split(',') if '\'' not in j and '"' not in j), im+10000 if i.style.getPropertyPriority('font-family') else im)
     for ele in bk.iter('font'):
         try:
             cmap = set(TTFont(ele.fp).getBestCmap())
@@ -63,16 +59,30 @@ def subfont(bk: book):
             ele = bk.set(bk.elem(fp))
         ele.cmap = cmap
     for ele in bk.iter('text'):
-        data = ele.read()
-        fmlparser(ele, ''.join(html_style.findall(data)))
-        dom = document_fromstring(
-            data.encode(), XHTMLParser(remove_comments=True))
-        for i in html_link.findall(data):
-            i = bk.get(bsn=first(getbsn(i)))
-            if hasattr(i, 'sel'):
-                ele.sel.update(i.sel)
-        for i, j, k in ele.sel:
-            pass
+        dom, ele.sel = document_fromstring(ele.read(True), XHTMLParser(remove_comments=True)), {}
+        for i in dom.xpath('//link'):
+            css = bk.get(bsn=first(getbsn(i.get('href'))))
+            if css:
+                for imp in css.imp:
+                    ele.sel.update(imp.sel)
+                ele.sel.update(css.sel)
+        for i in dom.xpath('//style'):
+            sheet=CSSParser(loglevel='CRITICAL', parseComments=False,validate=False).parseString(i.text).cssRules
+            for rule in sheet:
+                if rule.type == 1:
+                    fml = rule.style.fontFamily
+                    if fml:
+                        sel = rule.selectorText
+                        for part in css_splitsel.split(sel):
+                            im = 100
+                            if part.startswith(tuple('abcdefghijklmnopqrstuvwxyz*')):
+                                im += 1
+                            if css_clssel.search(part) or css_regsel.search(part):
+                                im += 10
+                            if css_idsel.search(part):
+                                im += 100
+                        if sel not in ele.sel or im>=ele.sel[sel][1]:
+                            ele.sel[sel] = (tuple(f.strip() for f in fml.split(',') if '\'' not in f and '"' not in f), im+10000 if rule.style.getPropertyPriority('font-family') else im)
         for i in dom.xpath('//*[contains(@style,"font-family")]'):
             m, n = html_line.search(i.get('style')).groups()
             ELEM[i] = [{i} | set(i.iterdescendants()), m, 7 if n else 3]
